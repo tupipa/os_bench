1e31852b4bea45a6c64e73cf496d32eb77fbef4e
Lele Ma <lma17@ur.rochester.edu>
2019-09-12 23:19:19 +0000

    cap from default; error:
    BAD_PAGE_FAULT: pid 641 tid 100055 (hello.exe), uid 0: pc 0x20478 got a read fault (type 0x2) at 0x8d0
    Trapframe Register Dump:
    $0: 0                  at: 0x48550            v0: 0x8d0              v1: 0x7ffffefaa0
    a0: 0                  a1: 0                  a2: 0                  a3: 0
    a4: 0                  a5: 0                  a6: 0                  a7: 0
    t0: 0x209fc            t1: 0x1c               t2: 0x7ffffef364       t3: 0x402b7810
    s0: 0x7ffffefb80       s1: 0x1                s2: 0x7ffffefb70       s3: 0
    s4: 0                  s5: 0                  s6: 0                  s7: 0
    t8: 0x1                t9: 0x20980            k0: 0                  k1: 0
    gp: 0x48020            sp: 0x7ffffef920       s8: 0x7ffffef920       ra: 0x2086c
    status: 0x408084b3 mullo: 0; mulhi: 0; badvaddr: 0x8d0
    cause: 0x8; pc: 0x20478
    $ddc: v:1 s:0 p:0007817d b:0000000000000000 l:0000008000000000 o:0 t:-1
    $c01: v:1 s:1 p:00068117 b:0000000000000000 l:0000008000000000 o:20450 t:1
    $c02: v:1 s:1 p:0007817d b:0000000000000000 l:0000008000000000 o:40270 t:1
    $c03: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c04: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c05: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c06: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c07: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c08: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c09: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c10: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c11: v:1 s:0 p:0007817d b:0000000000000000 l:0000008000000000 o:7ffffef970 t:-1
    $c12: v:1 s:0 p:00068117 b:0000000000000000 l:0000008000000000 o:40050000 t:-1
    $c13: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c14: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c15: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c16: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c17: v:1 s:0 p:00068117 b:0000000000000000 l:0000008000000000 o:209fc t:-1
    $c18: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c19: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c20: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c21: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c22: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c23: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c24: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c25: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c26: v:1 s:0 p:0007817d b:0000000000000000 l:0000008000000000 o:40270 t:-1
    $c27: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c28: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c29: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c30: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $c31: v:0 s:0 p:00000000 b:0000000000000000 l:ffffffffffffffff o:0 t:-1
    $pcc: v:1 s:0 p:00068117 b:0000000000000000 l:0000008000000000 o:20478 t:-1
    Page table info for bad address 0x8d0: pde = 0x9800000002862000, pte = 0
    pid 641 (hello.exe), jid 0, uid 0: exited on signal 11 (core dumped)
    root@qemu-cheri128-root:~ #

---
+++ cheritypes/sandbox_hello/hello.c
@@ -0,0 +1,132 @@
+
+#if !__has_feature(capabilities)
+#error "This code requires a CHERI-aware compiler"
+#endif
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cheri/cheri.h>
+#include <cheri/cheric.h>  // builtin wrappers
+#include <cheri/libcheri_type.h>  // libcheri type alloc()
+#include <cheri/libcheri_invoke.h>  // libcheri invoke prototype
+#include "cheritest.h"
+
+#include <machine/sysarch.h>
+
+extern void	sandbox_creturn(void);
+extern void	sandbox_creturn_end;
+//extern void __attribute__ ((cheri_ccallee)) sandbox_invoke(void * __capability c1, void* __capability c2);
+extern void __attribute__ ((cheri_ccall)) sandbox_invoke(void * __capability c1, void* __capability c2);
+//extern void sandbox_invoke(void * __capability c1, void* __capability c2);
+
+
+static void * __capability libcheri_sealing_root;
+
+static void *__capability sandbox_A_sealcap;
+static void *__capability sandbox_A_codecap;
+static void *__capability sandbox_A_datacap;
+
+struct sandbox_data{
+  int data __attribute__ ((aligned(16)));
+  char name[32] __attribute__ ((aligned(16)));
+};
+
+struct sandbox_data shared;
+struct sandbox_data privateA;
+struct sandbox_data privateB;
+
+struct sandbox_data *sharedp;
+struct sandbox_data *privateAp;
+struct sandbox_data *privateBp;
+
+void __attribute__((cheri_ccallee)) sandboxA_print(){
+  char a[32] __attribute__((aligned(16))) = "hello from sandbox A";
+  printf("%s\n", a);
+  //printf(("printing in sandbox A\n")__attribute__((aligned(16))));
+  //printf("HELLO A A A A A A A A \nA \nA \nA \nA \nA \n");
+  //printf("shared data: %d\n", sharedp->data);
+  //printf("A private data: %d\n", privateAp->data); 
+  //printf("B private data: %d\n", privateBp->data);
+  //sandboxA_end:
+}
+
+void sandboxB_print(){
+  printf("printing in sandbox B\n");
+  printf("shared data: %d\n", sharedp->data);
+  printf("A private data: %d\n", privateAp->data); 
+  printf("B private data: %d\n", privateBp->data);
+  //sandboxB_end:
+}
+
+
+/*
+ * One-type setup for ccall-related tests.
+ */
+void
+cheritest_ccall_setup(void)
+{
+
+	printf("begin.\n");
+
+  /** sandbox A **/
+
+	if (sysarch(CHERI_GET_SEALCAP, &libcheri_sealing_root) < 0)
+		libcheri_sealing_root = NULL;
+	assert((cheri_getperm(libcheri_sealing_root) & CHERI_PERM_SEAL) != 0);
+	assert(cheri_getlen(libcheri_sealing_root) != 0);
+
+#ifdef __CHERI_PURE_CAPABILITY__
+	sandbox_A_sealcap = libcheri_sealing_root;
+#else
+
+	sandbox_A_sealcap = libcheri_type_alloc();
+	//sandbox_A_sealcap = libcheri_sealing_root;
+
+#endif
+	sandbox_A_codecap = cheri_getpcc();
+	sandbox_A_codecap = cheri_setaddress(sandbox_A_codecap, 
+	   (vaddr_t)&sandboxA_print);
+	sandbox_A_codecap = cheri_seal(sandbox_A_codecap, sandbox_A_sealcap);
+
+	sandbox_A_datacap = cheri_getdefault();
+	sandbox_A_datacap = cheri_setaddress(sandbox_A_datacap, (vaddr_t)&privateA);
+	sandbox_A_datacap = cheri_seal(sandbox_A_datacap, sandbox_A_sealcap);
+
+  /**/
+
+	printf("done.");
+}
+
+
+void test_sandboxA( void )
+{
+	struct cheri_object co;
+
+	co.co_codecap = sandbox_A_codecap;
+	co.co_datacap = sandbox_A_datacap;
+	(void)libcheri_invoke(co, 0,
+	    0, 0, 0, 0, 0, 0, 0, 0,
+	    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+	//sandbox_invoke(sandbox_A_codecap, sandbox_A_datacap);
+	cheritest_success();
+}
+
+
+
+int main(void){
+
+ printf("hello world\n");
+
+ printf("cheritest setup\n");
+ cheritest_ccall_setup();
+
+ printf("now start testing...\n");
+
+ test_sandboxA();
+ printf("done test with sandboxA.\n");
+
+ return 0;
+
+}
+
